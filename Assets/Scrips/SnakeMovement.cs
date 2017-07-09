95f4bc1a9ae105cf304e1b0ac759625a9f44d9b8
Abhay Amin <pixoloproductions@gmail.com>
2017-07-09 02:50:24 -0700

    Touch Event

--- Assets/Scrips/SnakeMovement.cs
+++ Assets/Scrips/SnakeMovement.cs
@@ -28,6 +28,9 @@ public class SnakeMovement : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0)) {
 			PlayerRotate ();
 		}
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            PlayerRotateTouch();
+        }
 		PlayerMove ();
 
 	}
@@ -40,6 +43,15 @@ public class SnakeMovement : MonoBehaviour {
 		dir = (Camera.main.ScreenToWorldPoint (Input.mousePosition) - this.gameObject.transform.position);
 		dir.Normalize ();
 	}
+
+    void PlayerRotateTouch() {
+        playerPosition = Camera.main.WorldToViewportPoint(this.gameObject.transform.position);
+        mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
+        float angle = AngleBetweenTwoPoints(playerPosition, mouseOnScreen);
+        this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90));
+        dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.gameObject.transform.position);
+        dir.Normalize();
+    }
 
 	float AngleBetweenTwoPoints (Vector3 a, Vector3 b) {
 		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
