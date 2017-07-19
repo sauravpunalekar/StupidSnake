02e8d5c7f713617d48bba550541ca659a3d05209
Akshay Shah <akshah1997@gmail.com>
2017-07-19 22:32:08 +0530

    Improved Particle Effects Rendering
    Removed the collider as soon as player hits cheese. This leads to player able to cross the ptha occupied by cheese which was not the case earlier.

--- Assets/Scrips/PrtcleEfct.cs
+++ Assets/Scrips/PrtcleEfct.cs
@@ -6,28 +6,28 @@ public class PrtcleEfct : MonoBehaviour {
 
 	public Transform boom;
 	ParticleSystem cheese;
+	ParticleSystem.EmissionModule em;
 	 
 	// Use this for initialization
 	void Start () {
 		cheese = boom.GetComponent<ParticleSystem> ();
-		var em = cheese.emission;
+		em = cheese.emission;
 		em.enabled = false;
 	}
 
-	void OnCollisionEnter2D(Collision2D collision){
+	void OnCollisionEnter2D(Collision2D collision){		
 		if (collision.gameObject.name == "Player") {
+			GetComponent<PolygonCollider2D> ().enabled = false;
             GetComponent<AudioSource>().Play();
-            ScoreManager.currentscore++;
-			var em = cheese.emission;
 			em.enabled = true;
 			GetComponent<Renderer> ().enabled = false;
+			//ScoreManager.currentscore++;
 			StartCoroutine (StopBoom());
 		}
 	}
 
 	IEnumerator StopBoom() {
 		yield return new WaitForSeconds(0.8f);
-		var em = cheese.emission;
 		em.enabled = false;
 		Destroy(this.gameObject);
 	}
