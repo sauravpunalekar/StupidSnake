ae06b5bc3163ce2b24aea2ae60c2c9828307d0d4
sauravpunalekar <sauravpunalekar@gmail.com>
2017-07-23 16:41:13 +0530

    Added PowerManager

---
+++ Assets/Scrips/PowerManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerManager : MonoBehaviour {
+	public static int powerflag;
+	public static int power1_duration = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
